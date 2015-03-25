6f9b0f90e4e25c71a496bc59fc11095d6b317967
Rachel VanBoening <vanboeningrr@gmail.com>
2015-03-25 10:33:55 -0500

    Use correct params

--- controllers/user.rb
+++ controllers/user.rb
@@ -7,6 +7,7 @@ end
 post "/menu" do
   @user=User.create(params)
   @user.save
+  binding.pry
   erb :"/user/menu"
 end
 
@@ -14,13 +15,11 @@ end
 # Route for each drink option
 post "/user/drink/:drink_name" do
   binding.pry
-  @user_id=params["id"]
+  @user_id=params["user_id"]
   erb :"user/drink/#{params[:drink_name].downcase}_page"
 end  
 
 get "/success" do
-  user_id=params["user_id"]
-  @new_item=Item.new(params)
-  @new_item.insert(user_id)
+  @new_item=Item.create(params)
   erb :"user/success"
 end
